9eb3fc4119ad4fed18ef28743f67f5cc1544054a
Kriller <kriller83@hotmail.com>
2015-04-20 10:00:38 +0200

    Städat i koden

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div id="footer">
+  <button onclick="terms()">Terms and conditions</button>
+    <script>
+function terms() {
+    alert("hmmmm");
+}
+</script>
+
+    <button onclick="myFunction()">Contact</button>
+      <script>
+function myFunction() {
+    alert("Högskolan i Halmstad");
+}
+</script>
+
+| This site is copyrighted. All rights reserved 2014.</div>
