09b41d178eba15fff37f83eb8bc744893705f828
Kriller <kriller83@hotmail.com>
2015-04-20 10:01:53 +0200

    städ

--- index.php
+++ index.php
@@ -29,82 +29,59 @@ function MM_effectAppearFade(targetElement, duration, from, to, toggle)
 </head>
 
 
-
 <body>
 
 
 <div id="wrapper">
+  <div id="header"><a href="index.html"><img src="images/logo.png" /></a>
+    <div id="loginmain">
+      <a href="#" target="_blank">Forgot password?</a>
+        <form name="login" actions="login_check.php" method="post">
+          <input type="text" name="Email" placeholder="Email" />
+          <input type="text" name="Password" placeholder="Password" />
+          <input type="submit" name="submit_login" value="Login" />
+        </form>
+    </div>
 
-<div id="header"><a href="index.html"><img src="images/logo.png" /></a>
-
-
-<div id="loginmain">
-  <a href="#" target="_blank">Forgot password?</a>
-    <form name="login" actions="login_check.php" method="post">
-      <input type="text" name="Email" placeholder="Email" />
-      <input type="text" name="Password" placeholder="Password" />
-      <input type="submit" name="submit_login" value="Login" />
-    </form>
-</div>
-
-
+<!--Hej på dig-->
 </div>
 
 <div id="maincontent">
+  <div id="top_image"></div>
+    <div id="leftcolumn">
+      <h2>Welcome</h2>
+      <p>This site works as a community where you can upload, share, display and discuss your prototypes</p>
+      <p>Join us today, membership is free</p>
+    </div>
 
-<div id="top_image"></div>
+    <div id="middlecolumn">
+      <h2>Sign up!</h2>
+        <a href="www.google.se" rel="lightbox[kristian]" title="car prototype"><p>sign up</p></a>
+          <form name="new_user" actions="new_user.php" method="post">
+            <input type="text" name="Fname" placeholder="First Name" />
+            <input type="text" name="Lname" placeholder="Last Name" />
+            <input type="text" name="Email" placeholder="Email" />
+            <input type="text" name="Password2" placeholder="Password" />
+            <br>
+            <input type="submit" name="submit_reg" value="Sign up!" /> 
+          </form>
+       </div>
 
+     <div id="rightcolumn">
+      <h2>Prototype examples </h2>
+        <a href="slideshow/lightbox/images/frontone.jpg" rel="lightbox[kristian]" title="Building"><img src="slideshow/lightbox/images/frontone.jpg" width="72" height="72" /></a> 
+        <a href="slideshow/lightbox/images/fronttwo.jpg" rel="lightbox[kristian]" title="Iron man"><img src="slideshow/lightbox/images/fronttwo.jpg" width="72" height="72" /></a>
+        <a href="slideshow/lightbox/images/frontthree.jpg" rel="lightbox[kristian]" title="Starship"><img src="slideshow/lightbox/images/frontthree.jpg" width="72" height="72" /></a>
+        <a href="slideshow/lightbox/images/frontfour.jpg" rel="lightbox[kristian]" title="Startrooper"><img src="slideshow/lightbox/images/frontfour.jpg" width="72" height="72" /></a> 
+        <a href="slideshow/lightbox/images/frontfive.jpg" rel="lightbox[kristian]" title="Car"><img src="slideshow/lightbox/images/frontfive.jpg" width="72" height="72" /></a>
+        <a href="slideshow/lightbox/images/frontsix.jpg" rel="lightbox[kristian]" title="Mobile home"><img src="slideshow/lightbox/images/frontsix.jpg" width="72" height="72" /></a>
+     </div>
 
-<div id="leftcolumn">
-  <h2>Welcome</h2>
-  <p>This site works as a community where you can upload, share, display and discuss your prototypes</p>
-  <p>Join us today, membership is free</p>
-</div>
-
-
-
-<div id="middlecolumn">
-  <h2>Sign up!</h2>
-  <a href="www.google.se" rel="lightbox[kristian]" title="car prototype"><p>sign up</p></a>
-  <form name="new_user" actions="new_user.php" method="post">
-    <input type="text" name="Fname" placeholder="First Name" />
-    <input type="text" name="Lname" placeholder="Last Name" />
-    <input type="text" name="Email" placeholder="Email" />
-    <input type="text" name="Password2" placeholder="Password" />
-    <br>
-    <input type="submit" name="submit_reg" value="Sign up!" /> 
-  </form>
-</div>
-
-
-<div id="rightcolumn">
-  <h2>Prototype examples </h2>
-  
-  
-  
-  <a href="slideshow/lightbox/images/car.jpg" rel="lightbox[kristian]" title="car prototype"><img src="slideshow/lightbox/images/tn_test.jpg" width="72" height="72" /></a> 
-
-<a href="slideshow/lightbox/images/image-2.jpg" rel="lightbox[kristian]" title="picture Two"><img src="slideshow/lightbox/images/thumb-2.jpg" width="72" height="72" /></a>
-
-<a href="slideshow/lightbox/images/image-3.jpg" rel="lightbox[kristian]" title="picture Three"><img src="slideshow/lightbox/images/thumb-3.jpg" width="72" height="72" /></a>
-  
-<a href="slideshow/lightbox/images/car.jpg" rel="lightbox[kristian]" title="car prototype"><img src="slideshow/lightbox/images/tn_test.jpg" width="72" height="72" /></a> 
-
-<a href="slideshow/lightbox/images/image-2.jpg" rel="lightbox[kristian]" title="picture Two"><img src="slideshow/lightbox/images/thumb-2.jpg" width="72" height="72" /></a>
-
-<a href="slideshow/lightbox/images/image-3.jpg" rel="lightbox[kristian]" title="picture Three"><img src="slideshow/lightbox/images/thumb-3.jpg" width="72" height="72" /></a>
-  
-  
-  
-  
-</div>
-
-<div id="footer"><a href="#" target="_blank">Terms and conditions</a> | <a href="#">Contact</a> | This site is copyrighted. All rights reserved 2014.</div>
-
-
-</div></div>
-
-
-
-</body>
+<?php include'footer.php';?>
+    </div>
+  </body>
 </html>
+
+
+
+
