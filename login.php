612e64342b13d92013775b24ce242c5814973879
Kriller <kriller83@hotmail.com>
2015-04-27 14:54:11 +0200

    meh

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ echo $navigation;
 <div id="middlecolumn">
   <center><h3>&nbsp;</h3></center>
     <center>
-      <p><img src="images/webwords.jpg" width="72" height="72" /><br />
+      <p><img src="images/webwords.jpg" width="72" height="72" class="padtop12" /><br />
         Webdesign</p>
       <p><img src="images/4961518-939611-dimensional-cube-made-of-ones-and-zeros-isolated-on-white.jpg" width="72" height="72" class="padtop12" /><br />
         software
@@ -56,14 +56,11 @@ echo $navigation;
       <p><img src="images/bil.jpg" width="72" height="72" class="padtop12" /> <br />
         Cars</p>
       <p><img src="images/17789061-vector-illustration-of-blue-glossy-icon.jpg" width="72" height="72" class="padtop12" /><br />
-        Others
-    </center>
+        Others</center>
   </div>
-</div>
+
 
 <div id="footer"><a href="#" target="_blank">Terms and conditions</a> | <a href="#">Contact</a> | This site is copyrighted. All rights reserved 2014.</div>
-
-</div></div>
 
 </body>
 </html>
