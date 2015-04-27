bf749702f506932f41365350ef6c66b04f1c5ae0
Kriller <kriller83@hotmail.com>
2015-04-27 17:05:30 +0200

    eeh

---
+++ chat.php
@@ -0,0 +1,22 @@
+<?php include'template.php'; ?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>ProtoPro Chat Page</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+          <script type="text/javascript">
+        </script>
+      </head>
+
+<body onload="MM_preloadImages('images/button-signout.jpg')">
+  <div id="wrapper">
+    <div id="header2"><center>
+      <a href="index.html"><img src="images/logo.png" width="225" height="50" border="0" /></a>
+        </center></div>
+<?php
+echo $navigation;
+?>
+
