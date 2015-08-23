566a49352c56b18a27865d3d1647327c3bf4dc63
adamzki <aadammzkii@gmail.com>
2015-08-23 11:46:29 +0200

    ladda upp bild
    Nu funkar det att ladda upp bild

--- prototypes.php
+++ prototypes.php
@@ -19,7 +19,7 @@ END;
       if($res->num_rows > 0){
         for ($i=0; $i < 3; $i++) { 
          $row = $res->fetch_object();
-         echo '<div id="category_frame_latest"><a href="specific_prototype.php?protoid=' . $row->protoid . '"><img id="catogory_img" src="data:image/jpeg;base64,'.base64_encode($row->pic).'"><br>';
+         echo '<div id="category_frame_latest"><a href="specific_prototype.php?protoid=' . $row->protoid . '"><img id="catogory_img" src="' . $row->pic . '"><br>';
          echo $row->name . "<br></a></div>";
        
         }
--- upload.php
+++ upload.php
@@ -51,43 +51,82 @@
             <h3>Category:</h3>
             <select name="category">
               <?php
+                $cat_id = 0;
+
                 $query = <<<END
                 SELECT * FROM categories
 END;
                 $res = $mysqli->query($query);
-                while($row = $res->fetch_object()){
+                for ($i=1; $i < 5; $i++) { 
+                $row = $res->fetch_object();
+
                   echo "<option value='{$row->name}'> $row->name";
-                  $cat_id = $row->cat_id;
-                }
+                  echo "<input type='hidden' name='cat_id' value='$cat_id+$i'>";
+                  
+                    
+                  
+                  }
+                
               ?>
             </select>
             
             <h3>Picture:</h3>
-            <input type="file" name="image">
+            <input type="file" name="fileToUpload" id="fileToUpload">
             <br><br>
             <input type="submit" name="upload" value="Upload">
           </form>
 
 <?php
-              if(isset($_POST['upload'])){
-                $image_name = $_FILES['image']['name'];
-                $image_type = $_FILES['image']['type'];
-                $image_size = $_FILES['image']['size'];
-                $image_tmp_name = $_FILES['image']['tmp_name'];
-                $pic = file_get_contents($_FILES['image']['tmp_name']);
+  $target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+        $image_name = $_FILES['image']['name'];
+        
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 200000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
 
-              if(isset($image_name)){
-                $query2 = <<<END
-                INSERT INTO prototypes (name,description,pic,Userid,cat_id)
-                VALUES ('{$_POST['name']}', '{$_POST['description']}', '{$pic}', '{$_SESSION['Userid']}', '{$cat_id}')
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      $image_tmp_name = basename( $_FILES["fileToUpload"]["name"]);
+      $query = <<<END
+      SELECT * FROM categories
+      WHERE cat_id = 
 END;
-              $mysqli->query($query2);
-              echo "Your prototype has been uploaded and stored in our database!";
-              }else{
-                echo "Fill out all fields to upload a prototype";
-              }
-              }
-            ?>
+      $query = <<<END
+      INSERT INTO prototypes(name,description,Userid,cat_id,pic)
+      VALUES ('{$_POST['name']}','{$_POST['description']}','{$_SESSION['Userid']}','{$_POST['cat_id']}','uploads/{$image_tmp_name}')
+END;
+      $mysqli->query($query);
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
     </div>
     <div id="double-right-column">
       <h2><center>My Page</center><h2>
@@ -100,7 +139,7 @@ END;
   $res = $mysqli->query($query);
   if($res->num_rows > 0){
     while($row = $res->fetch_object()){
-      echo '<div id="category_frame"><a href="specific_prototype.php?protoid=' . $row->protoid . '"><img id="catogory_img" src="data:image/jpeg;base64,'.base64_encode($row->pic).'"><br>';
+      echo '<div id="category_frame"><a href="specific_prototype.php?protoid=' . $row->protoid . '"><img id="catogory_img" src="' . $row->pic . '"><br>';
       echo $row->name . "</a></div>";
     }
   }
